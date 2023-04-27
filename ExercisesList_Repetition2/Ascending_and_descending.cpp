6afdd17ffbd8d0c2ceec3fddbd79b1c9c061c6ca
BrunoNT10 <brunont1211@gmail.com>
2023-04-26 21:51:54 -0300

    Ascendind an descending exercise

---
+++ ExercisesList_Repetition2/Ascending_and_descending.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int X, Y;
+	
+	do{
+    	cin >> X >> Y;
+		if (X > Y){
+			cout << "Decrescente" << endl;
+		}		
+		else if(X < Y){
+			cout << "Crescente" << endl;
+		}
+	}
+	while(X != Y);
+		
+	return 0;
+}
